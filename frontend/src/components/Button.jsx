c522be6bbacebce745047295e49f3a9dbb7842b0
Abhishek Singh <abhisheksingh2532001@gmail.com>
2025-07-16 02:30:54 +0530

    update

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
-export const Button=({label,onClick})=>{
+export const Button=({label,onClick,className})=>{
     return (
-        <div className="mt-2">
+        <div className={`mt-2 ${className}`}>
             <button className="w-full text-center p-2 px-3 rounded-lg text-white bg-gray-800" onClick={onClick}>{label}</button>
         </div>
     )
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,11 +3,16 @@ import { useNavigate } from "react-router-dom"
 export const HomePage=()=>{
     const navigate=useNavigate();
     return (
-        <div>
-            <div>Payme</div>
-            <Button label="sign in" onClick={()=>{
-                navigate("/signin");
-            }}/>
+        <div className="bg-slate-300 flex flex-col w-full h-screen justify-center items-center">
+            <div className="flex flex-col justify-center items-center rounded-lg w-100 h-60 bg-white">
+
+                <div className="text-lg font-light mt-4">welcome to </div>
+                <div className="text-2xl font-bold mb-4">Payme</div>
+                <Button className="w-50" label="sign in" onClick={()=>{
+                    navigate("/signin");
+                }}/>
+            </div>
+           
         </div>
     )
 }
